ac3df47119ea0909ea66dc589663631bdf132361
Cender <CenderG@win.dowjones.net>
2018-03-19 21:48:11 -0400

    my message

---
+++ assets/scripts/data.js
@@ -0,0 +1,24 @@
+var trending =[
+  "Aviation", 
+"Bloody Mary", 
+"Boulevardier", 
+"Caipirinha", 
+"Daiquiri", 
+"Gimlet", 
+"Gin and Tonic", 
+"Gin Fizz", 
+"Mai Tai", 
+"Manhattan", 
+"Margarita", 
+"martini", 
+"Mojito", 
+"Moscow Mule", 
+"Negroni", 
+"Old Fashioned", 
+"Pisco Sour", 
+"Sazerac", 
+"Tom Collins", 
+"Whiskey Sour", 
+"Zombie"
+];
+
